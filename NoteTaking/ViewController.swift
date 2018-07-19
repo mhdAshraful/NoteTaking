a4b4d69a1ab977657f5aa182de63a3877df1ca55
Ashraf <mycdu.edu@icloud.com>
2018-07-19 20:47:55 +0930

    Initial Commit

---
+++ NoteTaking/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NoteTaking
+//
+//  Created by Md Ashraful Islam on 19/7/18.
+//  Copyright © 2018 Md Ashraful Islam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
